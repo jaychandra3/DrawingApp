0621cd732a5ab93eac88d87c6917c6f112eccf2e
Jay Chandra <jaychandra@college.harvard.edu>
2020-06-13 14:39:49 -0600

    New App

---
+++ Neuro Platform/Neuro Platform/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Neuro Platform
+//
+//  Created by Jay Chandra on 6/13/20.
+//  Copyright © 2020 NDDP. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
