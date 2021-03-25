d951fe4f739b09ac65f4215b0da40048691cee19
Jason Shang <jasonshang2002@gmail.com>
2021-03-24 22:06:21 -0400

    updated NewPatientView entry form - Phillip and Jason

--- AlzApp/Neuro Platform/NavigationScreens/NewPatientView.swift
+++ AlzApp/Neuro Platform/NavigationScreens/NewPatientView.swift
@@ -12,33 +12,69 @@ import SwiftUI
 struct NewPatientView: View {
     @State private var name: String = ""
     @State private var age: String = ""
+    @State private var sex: String = "Not Selected"
+    @State private var hand: String = "Not Selected"
     
     @State private var isActive : Bool = false
     @State private var showingAlert = false
     
     var body: some View {
+        
         VStack {
-            Text("New Patient")
-                .textStyle(TitleTextStyle())
-            Spacer()
-            Text("Patient Name")
-            TextField("Enter Patient Name", text: $name)
-            Text("Patient Age")
-            TextField("Enter Patient Age", text: $age)
-            NavigationLink(destination: DrawingView(rootIsActive: $isActive, trials: 3, patient: name), isActive : $isActive) {
-                EmptyView()
-            }.isDetailLink(false)
-            Button("Start Task") {
-                if(name.count > 0) {
-                    self.isActive.toggle()
-                } else {
-                    self.showingAlert.toggle()
+            VStack{
+                Text("New Patient")
+                    .textStyle(TitleTextStyle())
+
+                HStack{
+                    Text("Patient Name")
+                    TextField("Enter Patient Name", text: $name)
+                }.padding()
+                HStack{
+                    Text("Patient Age")
+                    TextField("Enter Patient Age", text: $age)
+                }.padding()
+                
+                HStack {
+                    Text("Biological Sex")
+                    VStack{
+                        Picker(selection: $sex, label: Text("Sex")) {
+                            Text("Male").tag("Male")
+                            Text("Female").tag("Female")
+                        }.pickerStyle(SegmentedPickerStyle())
+                    }
+                }.padding()
+            }
+            
+            VStack {
+                HStack {
+                    Text("Dominant Hand")
+                    Picker(selection: $hand, label: Text("Dominant Hand")) {
+                        Text("Left Hand").tag("Left Hand")
+                        Text("Right Hand").tag("Right Hand")
+                    }.pickerStyle(SegmentedPickerStyle())
                 }
-            }.alert(isPresented: $showingAlert, content: {
-                Alert(title: Text("Form Incomplete"), message: Text("Please enter a name"), dismissButton: .default(Text("Ok")))
-            })
-            Spacer()
-        }.padding()
+                
+                NavigationLink(destination: DrawingView(rootIsActive: $isActive, trials: 3, patient: name), isActive : $isActive) {
+                    EmptyView()
+                }.isDetailLink(false)
+                
+                
+                Button("Start Task") {
+                    if(name.count > 0 &&
+                        age.count > 0 &&
+                        sex != "Not Selected" &&
+                        hand != "Not Selected") {
+                        self.isActive.toggle()
+                    } else {
+                        self.showingAlert.toggle()
+                    }
+                }.alert(isPresented: $showingAlert, content: {
+                    Alert(title: Text("Form Incomplete"),
+                          message: Text("Please check each entry or selection"),
+                          dismissButton: .default(Text("Ok")))
+                }).padding()
+            }.padding()
+        }
     }
 }
 
