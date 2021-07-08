82bb6fb102eac6064bbd85ad54646f81d9f5121c
Jason Shang <jasonshang2002@gmail.com>
2021-07-07 17:48:32 -0700

    about page changes

--- AlzApp/Neuro Platform/NavigationScreens/AboutAppView.swift
+++ AlzApp/Neuro Platform/NavigationScreens/AboutAppView.swift
@@ -11,23 +11,23 @@ import SwiftUI
 // Barebones AboutAppView : TODO
 struct AboutAppView: View {
     var body: some View {
-        VStack {
-            Spacer()
-            Image("gami_logo").resizable().scaledToFit().padding()
-            
-            Text("About the App")
-                .textStyle(TitleTextStyle()).padding()
-            Text("[Analysis Platform] is a mobile neurodegenerative disease screening platform developed by a team of students and faculty from the Global Alliance for Medical Innovation (GAMI).\n\nHere at GAMI, we are deeply concerned by the severe shortage of healthcare resources the world is facing. Those who are most in need of help are often ignored by the major players in the medical space. GAMI aims to leverage the technical expertise and passion of students around the world as well as the invaluable experience of healthcare professionals to develop impactful technologies, policies, and programs and make healthcare accessible to all.\n\nOur team represents a diverse group of undergraduate and graduate students from different academic backgrounds (computer science, statistics, engineering, neuroscience, and biology) from multiple universities in the US and abroad (Duke University, Harvard University, Singapore University of Technology & Design). The team advisors from Harvard School of Engineering and Applied Sciences and from Harvard Medical School Teaching Hospitals bring expertise in the mathematical analysis of physical phenomena such as handwriting, gait, and eye movement and clinical knowledge in neurodegenerative disease treatment. Special thanks to Dr. Mahadevan, Dr. Press, Dr. Lim, and Dr. Shaughnessy for their extensive feedback and support on the project!")
-                .textStyle(BodyTextStyle()).padding()
-            
-            HStack {
-                Image("harvard_logo").resizable().scaledToFit().padding()
-                Image("duke_logo").resizable().scaledToFit().padding()
-                Image("sutd_logo").resizable().scaledToFit().padding()
-                Image("bidmc_logo").resizable().scaledToFit().padding()
+        ScrollView {
+            VStack {
+                Spacer()
+                Image("gami_logo").resizable().aspectRatio(contentMode: .fit).frame(width:UIScreen.screenWidth/2).padding()
+                Text("About the App")
+                    .textStyle(TitleTextStyle()).padding()
+                Text("[Analysis Platform] is a mobile neurodegenerative disease screening platform developed by a team of students and faculty from the Global Alliance for Medical Innovation (GAMI).\n\nHere at GAMI, we are deeply concerned by the severe shortage of healthcare resources the world is facing. Those who are most in need of help are often ignored by the major players in the medical space. GAMI aims to leverage the technical expertise and passion of students around the world as well as the invaluable experience of healthcare professionals to develop impactful technologies, policies, and programs and make healthcare accessible to all.").textStyle(BodyTextStyle()).padding()
+                Text("Our team represents a diverse group of undergraduate and graduate students from different academic backgrounds (computer science, statistics, engineering, neuroscience, and biology) from multiple universities in the US and abroad (Duke University, Harvard University, Singapore University of Technology & Design). The team advisors from Harvard School of Engineering and Applied Sciences and from Harvard Medical School Teaching Hospitals bring expertise in the mathematical analysis of physical phenomena such as handwriting, gait, and eye movement and clinical knowledge in neurodegenerative disease treatment. Special thanks to Dr. Mahadevan, Dr. Press, Dr. Lim, and Dr. Shaughnessy for their extensive feedback and support on the project!")
+                    .textStyle(BodyTextStyle()).padding()
+                
+                HStack {
+                    Image("harvard_logo").resizable().scaledToFit().padding()
+                    Image("duke_logo").resizable().scaledToFit().padding()
+                    Image("sutd_logo").resizable().scaledToFit().padding()
+                    Image("bidmc_logo").resizable().scaledToFit().padding()
+                }.padding()
             }.padding()
-            
-            Spacer()
         }
     }
 }
--- AlzApp/Neuro Platform/NavigationScreens/InstructionsView.swift
+++ AlzApp/Neuro Platform/NavigationScreens/InstructionsView.swift
@@ -11,13 +11,15 @@ import SwiftUI
 // Barebones Instruction View : TODO
 struct InstructionsView: View {
     var body: some View {
-        VStack {
-            Spacer()
-            Text("Instructions")
-                .textStyle(TitleTextStyle()).padding()
-            Text("1. Select the appropriate test to be administered (Parkinson’s or Alzheimer’s) based on the patient demographic.\n\n2. Enter the patient information (ID, dominant hand) into the fields and verify that the information is correct.\n\n3. Press “Start Task” to continue.\n\n4. Hand the device to the patient and help them draw using the Apple pencil on the practice screen. Remind patient to treat the iPad just like a piece of paper, and write naturally.\n\n5. Read and execute the instructions displayed on each step, using the “Next Trial” button to proceed to the next step.\n\n6. Press “Finish Test” once the last step is complete to return to the Home Screen.\n\n7. Press “View Patient Records” to access patient information and test data.\n\n8. Press “Export All Files” to transfer the patient records to local storage. ")
-                .textStyle(BodyTextStyle()).padding()
-            Spacer()
+        ScrollView {
+            VStack {
+                Spacer()
+                Text("Instructions")
+                    .textStyle(TitleTextStyle()).padding()
+                Text("1. Select the appropriate test to be administered (Parkinson’s or Alzheimer’s) based on the patient demographic.\n\n2. Enter the patient information (ID, dominant hand) into the fields and verify that the information is correct.\n\n3. Press “Start Task” to continue.\n\n4. Hand the device to the patient and help them draw using the Apple pencil on the practice screen. Remind patient to treat the iPad just like a piece of paper, and write naturally.\n\n5. Read and execute the instructions displayed on each step, using the “Next Trial” button to proceed to the next step.\n\n6. Press “Finish Test” once the last step is complete to return to the Home Screen.\n\n7. Press “View Patient Records” to access patient information and test data.\n\n8. Press “Export All Files” to transfer the patient records to local storage. ")
+                    .textStyle(BodyTextStyle()).padding()
+                Spacer()
+            }
         }
     }
 }
